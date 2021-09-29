76b66a72a7f39e51fa8868ee302e72cecda94aac
codemaster <celiel13@gmail.com>
2021-09-29 17:10:22 -0400

    editing code

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dejate llevar para que sientas la energia
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://facebook.com/castillolacone"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sigue la energia
         </a>
       </header>
     </div>
